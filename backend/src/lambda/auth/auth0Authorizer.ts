8c1c8e71b2f64649de5a0e70ccd4ee81d05870cb
idyol <ihssandk@gmail.com>
2023-01-15 00:58:01 +0100

    implement JWT verification

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -14,9 +14,7 @@ const logger = createLogger('auth')
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = 'https://dev-n8fb50pmsffem7gw.eu.auth0.com/.well-known/jwks.json'
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -58,11 +56,27 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  // Get the JSON Web Key Set (JWKS)
+  const jwks = await Axios.get(jwksUrl);
+
+  // Extract the public key from the JWKS
+  const signingKey = jwks.data.keys[0].x5c[0];
+  const jwk =  { kty: "RSA", n: signingKey, e: "AQAB" };
+
+  // Verify the JWT token
+  const decoded = verify(token, jwk, { algorithms: ['RS256'] }) as Jwt;
+
+  // Verify that the token is not expired
+  if (decoded.exp * 1000 < Date.now()) {
+    throw new Error('Token expired');
+  }
+
+  // Extract the payload (claims) from the JWT token
+  const payload = decoded.payload as JwtPayload;
+
+  return payload;
 }
+
 
 function getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
